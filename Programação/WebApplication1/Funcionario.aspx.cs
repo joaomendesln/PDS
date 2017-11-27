33182c4d458f43279d388fe9c67dba5296064491
João Mendes <mendeslopes.joao@gmail.com>
2017-11-27 17:28:46 -0300

    Update Funcionario.aspx.cs

--- Programação/WebApplication1/Funcionario.aspx.cs
+++ Programação/WebApplication1/Funcionario.aspx.cs
@@ -8,10 +8,51 @@ using System.Web.UI.WebControls;
 namespace WebApplication1
 {
     public partial class Funcionario : System.Web.UI.Page
-    {
+    {       
+        int idFuncionario;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void motoristaLabel_PreRender(object sender, EventArgs e)
+        {
+            if ((sender as Label).Text == "False") (sender as Label).Text = "Não";
+            else (sender as Label).Text = "Sim";
+        }
+
+        protected void tecnicoLabel_PreRender(object sender, EventArgs e)
+        {
+            if ((sender as Label).Text == "False") (sender as Label).Text = "Não";
+            else (sender as Label).Text = "Sim";
+        }
+
+        protected void idFuncionarioLabel_PreRender(object sender, EventArgs e)
+        {
+            idFuncionario = int.Parse((sender as Label).Text);
+            (sender as Label).Visible = false;
+        }
+
+        protected void LinkButtonEditar_PreRender(object sender, EventArgs e)
+        {
+            (sender as LinkButton).CommandName = Convert.ToString(idFuncionario);
+        }
+
+        protected void LinkButtonExcluir_PreRender(object sender, EventArgs e)
+        {
+            (sender as LinkButton).CommandName = Convert.ToString(idFuncionario);
+        }
+
+        protected void LinkButtonEditar_Click(object sender, EventArgs e)
+        {
+            Session["idFuncionarioEditar"] = (sender as LinkButton).CommandName;
+            Response.Redirect("~\\FuncionarioEditar.aspx");  
+        }
+
+        protected void LinkButtonExcluir_Click(object sender, EventArgs e)
+        {
+            Session["idFuncionarioExcluir"] = (sender as LinkButton).CommandName;
+            Response.Redirect("~\\FuncionarioExcluir.aspx");        
         }
     }
 }
